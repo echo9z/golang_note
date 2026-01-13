515a436eddb03b734839d928e268582337e734f0
echo9z <1131223522@qq.com>
2026-01-14 01:50:14 +0800

    vault backup: 2026-01-14 01:50:14

--- 02_0-base/06_basetype/03_string_2.go
+++ 02_0-base/06_basetype/03_string_2.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-
+	"fmt"
+	"strings"
 )
 
 func main() {
 	// 字符串分割
 	// Split：根据指定的分隔符将字符串分割成多个子串，并返回一个切片。
-	
+	str := "a,b,c,d,e"
+	// 按照指定字符进行分割
+	res = strings.Split(str, ",")
+	// 返回字符串切片
+	fmt.Println()
 	
 }
