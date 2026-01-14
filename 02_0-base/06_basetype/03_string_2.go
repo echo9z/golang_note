4e813db91712b29256d1993f3bc92443fe29eaa6
echo9z <1131223522@qq.com>
2026-01-14 14:48:00 +0800

    vault backup: 2026-01-14 14:48:00

--- 02_0-base/06_basetype/03_string_2.go
+++ 02_0-base/06_basetype/03_string_2.go
@@ -10,7 +10,7 @@ func main() {
 	// Split：根据指定的分隔符将字符串分割成多个子串，并返回一个字符串切片。
 	str := "a,b,c,d,e"
 	// 按照指定字符进行分割
-	let res string[] = strings.Split(str, ",")
+	var res []string = strings.Split(str, ",")
 	// 返回字符串切片
 	fmt.Println(res)
 	
